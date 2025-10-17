dbf3e8fa274ab72ef3ae5a151310a31d89ab1f69
Mounika <2300032943cseird@gmail.com>
2025-10-17 21:57:33 +0530

    updated jenkinsfile

--- doctor-management-react/src/config.js
+++ doctor-management-react/src/config.js
@@ -1,6 +1,5 @@
 const config = {
-    // url: "http://localhost:3000",
-    url: "http://localhost:2030/doctormanagementapi"
-    
+    url: "http://localhost:2030/doctormanagementapi",
+    // url: "http://localhost:3000"
 };
 export default config;
